1a53ffebcd96f91e9f999ab124f7b3d2c426fb4d
hongky <hongkypcx.hp@gmail.com>
2020-08-14 17:48:37 +0700

    change route

--- src/views/pages/info/more/index.vue
+++ src/views/pages/info/more/index.vue
@@ -1,13 +1,23 @@
 <template>
-    <div class="bg-blue-500 overflow-y-scroll h-full content-between flex justify-center items-center">
-            <router-link :to="'/about'">
-                 <vs-button
-                    danger
-                    border
-                >
-                    About me
-                </vs-button>
-            </router-link>
+    <div class="page bg-purple-500 overflow-y-scroll content-between ">
+        <div class="flex flex-wrap sm:flex-no-wrap justify-center items-center  ">
+            <div class="w-full sm:w-1/2  sm:h-full sm:pl-20 pt-20 sm:pt-0">
+                <h1 class="text-4xl text-gray-700">More</h1>
+                <h1 class="text-xl text-white">Want to know more about me? <span v-bot:text.init="'CITY Plan You may use special comments to disable some warnings.Use <br/>Use to ignore all warnings in a file.'"></span></h1>
+                <router-link class="flex items-center justify-center" :to="'/about'">
+                    <vs-button
+                        dark
+                        border
+                    >
+                        About me
+                    </vs-button>
+                </router-link>
+            </div>
+            <div class="w-full sm:w-1/2 flex flex-col justify-center items-start py-20 space-y-4">
+                <lottie path="city" :loop="true" :autoPlay="true" :loopDelayMin="0" :loopDelayMax="0" :speed="1" :height="300" :width="400" />
+            </div>
+        </div>
+            
     </div>
 </template>
 
--- src/views/pages/info/service/index.vue
+++ src/views/pages/info/service/index.vue
@@ -1,6 +1,14 @@
 <template>
-     <div class="bg-green-500 overflow-y-scroll h-full content-between z-50">
-            More thing going on here
+     <div class="page bg-green-500 overflow-y-scroll content-between">
+           <div class="flex flex-wrap sm:flex-no-wrap justify-center items-center  ">
+            <div class="w-full sm:w-1/2  sm:h-full sm:pl-20 pt-20 sm:pt-0">
+                <h1 class="text-4xl text-gray-700">Services</h1>
+                <h1 class="text-xl text-white">are you looking for services, Like any services? <span v-bot:text.init="'CITY Plan You may use special comments to disable some warnings.Use <br/>Use to ignore all warnings in a file.'"></span></h1>
+            </div>
+            <div class="w-full sm:w-1/2 flex flex-col justify-center items-start py-20 space-y-4">
+                <lottie path="underconstruction" :loop="true" :autoPlay="true" :loopDelayMin="0" :loopDelayMax="0" :speed="1" :height="400" :width="600" />
+            </div>
+        </div>
     </div>
 </template>
 
